e27ffc19d3a5cd07c790e19cbae9983a3f90c66b
Diego Pagini <ext_atsistemas10@es.gestamp.com>
2024-02-05 15:20:29 +0100

    stream provider

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -2,4 +2,5 @@ export './family_provider.dart';
 export './future_providers.dart';
 export './hello_world_provider.dart';
 export './state_providers.dart';
+export './stream_provider.dart';
 export './todos_provider.dart';
--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/stream_provider.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -9,6 +11,41 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final users = ref.watch(usersInChatProvider);
+
+    if (users.isLoading) {
+      return const Expanded(child: Center(child: CircularProgressIndicator()));
+    }
+
+    if (users.hasError) {
+      return Expanded(
+        child: Center(
+          child: Text('${users.error}'),
+        ),
+      );
+    }
+
+    final value = users.value!;
+
+    return ListView.builder(
+      itemCount: value.length,
+      itemBuilder: (context, index) {
+        final user = value[index];
+
+        return ListTile(
+          title: Text(user),
+        );
+      },
     );
   }
 }
